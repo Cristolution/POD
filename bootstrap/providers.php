0c588fd8e8355045f812a20ea795f084b520ffa1
cristolution <cyaghjian2004@outlook.com>
2026-08-24 16:49:23 +0300

    feat(admin): install Filament v4 admin panel

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Providers\AppServiceProvider;
+use App\Providers\Filament\AdminPanelProvider;
 use L5Swagger\L5SwaggerServiceProvider;
 
 return [
     AppServiceProvider::class,
+    AdminPanelProvider::class,
     L5SwaggerServiceProvider::class,
 ];
